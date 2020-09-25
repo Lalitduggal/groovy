b9394907171c8024e3c552c9b50288324f9c1ecc
lalit duggal <Lalit.duggal@gmail.com>
2020-09-25 13:46:19 +1000

    Update one.groovy

--- vars/one.groovy
+++ vars/one.groovy
@@ -26,3 +26,8 @@ def two(message){
 def mft_app_server() {
   sh '/usr/local/bin/aws cloudformation list-stacks'
 }
+
+def create_sns_topic(ENVIRONMENT_NAME,ENVIRONMENT_TYPE) {
+	sh '/usr/local/bin/aws cloudformation create-stack --stack-name ${ENVIRONMENT_NAME}-${ENVIRONMENT_TYPE}-mft-sns-cf-stack --template-url https://sandpit-lalit-cf-templates.s3-ap-southeast-2.amazonaws.com/mft-common/01-create-mft-sns-cf-stack.yml --parameters ParameterKey=EnvironmentNameP,ParameterValue=${ENVIRONMENT_NAME} ParameterKey=MftStackTypeP,ParameterValue=${ENVIRONMENT_TYPE}'
+	sh '/usr/local/bin/aws cloudformation wait stack-create-complete --stack-name ${ENVIRONMENT_NAME}-${ENVIRONMENT_TYPE}-mft-sns-cf-stack'
+}
